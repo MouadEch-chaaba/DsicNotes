777aa4a286f5f6ce9d329e769b7bd21d4ec521e3
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-02-20 22:46:17 +0000

    Adding the User bean

---
+++ src/com/dsic/beans/User.java
@@ -0,0 +1,70 @@
+package com.dsic.beans;
+
+import com.dsic.annotations.TableColumnName;
+import com.dsic.annotations.TableName;
+
+@TableName("Utilisateurs")
+public class User {
+	
+	@TableColumnName("Id")
+	private int identifier;
+	
+	@TableColumnName("login")
+	private String login;
+	
+	@TableColumnName("pass")
+	private String password;
+	
+	@TableColumnName("firstname")
+	private String firstName;
+	
+	@TableColumnName("lastname")
+	private String lastName;
+	
+	
+	public User(){
+		
+	}
+
+	public int getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(int identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+}
