f965166a07d8e20221373860370de51b2d1bd584
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-02-20 22:50:13 +0000

    Adding the Note bean

---
+++ src/com/dsic/beans/Note.java
@@ -0,0 +1,58 @@
+package com.dsic.beans;
+
+import com.dsic.annotations.TableColumnName;
+import com.dsic.annotations.TableName;
+
+@TableName("notes")
+public class Note {
+	
+	@TableColumnName("Id")
+	private int identifier;
+	
+	private String content;
+	
+	private String importance;
+	
+	@TableColumnName("id_user")
+	private int owner;
+	
+	public Note(){
+		
+	}
+
+	public int getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(int identifier) {
+		this.identifier = identifier;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getImportance() {
+		return importance;
+	}
+
+	public void setImportance(String importance) {
+		this.importance = importance;
+	}
+
+	public int getOwner() {
+		return owner;
+	}
+
+	public void setOwner(int owner) {
+		this.owner = owner;
+	}
+	
+	
+	
+	
+}
