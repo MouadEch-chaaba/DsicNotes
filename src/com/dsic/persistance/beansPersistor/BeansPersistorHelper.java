7ab1b09af65e0fc9e52c2c586b38baa1fcf65c9c
Mouad Ech-chaaba <mouad.echchaaba@gmail.com>
2016-02-21 16:32:36 +0000

    Adding {BeansPersistor} and {BeansPersistorHelper} classes that will deal with the persistance of the beans in the Database

---
+++ src/com/dsic/persistance/beansPersistor/BeansPersistorHelper.java
@@ -0,0 +1,46 @@
+package com.dsic.persistance.beansPersistor;
+
+import java.lang.reflect.Field;
+
+import com.dsic.annotations.PrimaryKey;
+import com.dsic.persistance.interfaces.IPersistentBean;
+
+public class BeansPersistorHelper {
+	
+	public static boolean isReadyToDelete(IPersistentBean bean) throws IllegalArgumentException, IllegalAccessException{
+		
+		boolean isReady = false;
+		
+		Class<? extends IPersistentBean> currentBeanClass = bean.getClass();
+		
+		Field[] currentBeanFields = currentBeanClass.getDeclaredFields();
+		
+		for(Field field : currentBeanFields){
+			field.setAccessible(true);
+			
+			if(field.get(bean) == null){
+				isReady = true;
+			}else{
+				isReady = false;
+				break;
+			}
+		}
+		
+		return isReady;
+	}
+	
+	public static void prepareForDelete(IPersistentBean bean) throws IllegalArgumentException, IllegalAccessException{
+		Class<? extends IPersistentBean> currentBeanClass = bean.getClass();
+		
+		Field[] currentBeanFields = currentBeanClass.getDeclaredFields();
+		
+		for(Field field : currentBeanFields){
+			field.setAccessible(true);
+			
+			if(!field.isAnnotationPresent(PrimaryKey.class)){
+				field.set(bean, null);
+			}
+		}
+	}
+	
+}
